1fc53e61bb706deb358138c81a1f29bff7089a6a
shariyerShazan <shariyershazan1@gmail.com>
2025-10-11 05:14:43 +0600

    -

--- src/app.service.ts
+++ src/app.service.ts
@@ -3,6 +3,7 @@ import { Injectable } from '@nestjs/common';
 @Injectable()
 export class AppService {
   getHello(): string {
-    return 'Hello World!';
+    // eslint-disable-next-line prettier/prettier
+    return "Shazan's eCommerce app!";
   }
 }
