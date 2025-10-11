536b1f334d95e6f3d0a72d4cfdb5505bb242c25f
shariyerShazan <shariyershazan1@gmail.com>
2025-10-11 23:13:46 +0600

    prisma add

---
+++ src/auth/auth.service.ts
@@ -0,0 +1,16 @@
+/* eslint-disable prettier/prettier */
+import { Injectable } from "@nestjs/common";
+
+
+@Injectable({})
+export class AuthService {
+    register(){
+        
+    }
+
+    login(){
+
+    }
+}
+
+// const service = new AuthService()
