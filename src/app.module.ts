78432530767798fdc37e2a85d2fce9a7c77bb210
shariyerShazan <shariyershazan1@gmail.com>
2025-10-14 10:26:22 +0600

    -

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,11 +3,12 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { UsersModule } from './users/users.module';
+import { AdminService } from './admin/admin.service';
 
 
 @Module({
   imports: [UsersModule],
   controllers: [AppController ],
-  providers: [AppService ],
+  providers: [AppService, AdminService ],
 })
 export class AppModule {}
